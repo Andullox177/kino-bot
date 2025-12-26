6478d18f0903e383ca68858f578502bb63219035
Andullox177 <mamacatstorytime@gmail.com>
2025-12-26 22:38:15 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+BOT_TOKEN = "7356647239:AAF4QyMxRA13sWYZ1Otf-LHcxvIcVkcN4gE"
+
+movies = {
+    "K100": "https://archive.org/details/night_of_the_living_dead",
+    "K101": "https://archive.org/details/charlie_chaplin-the-kid"
+}
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🎬 Kino kodini yuboring\nMasalan: K100"
+    )
+
+async def get_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    code = update.message.text.upper()
+
+    if code in movies:
+        await update.message.reply_text(f"🎥 Kino:\n{movies[code]}")
+    else:
+        await update.message.reply_text("❌ Bunday kod topilmadi")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get_movie))
+
+app.run_polling()
